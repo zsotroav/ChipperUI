a681602d6f6a3fa8f7d261941c5a72ed05c190cb
zsotroav <zsotroav@gmail.com>
2021-08-16 14:19:29 +0200

    minor update

--- ChipperUI/External.cs
+++ ChipperUI/External.cs
@@ -7,7 +7,7 @@ namespace ChipperUI
     class External
     {
         public static readonly string WritePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        public static readonly string MainPath = Path.Combine(WritePath, "zsotroav", "chiper");
+        public static readonly string MainPath = Path.Combine(WritePath, "zsotroav", "chipper");
 
         public static void SaveBin(string loc, byte[] Data)
         {
--- DotnetPublish.bat
+++
@@ -1,2 +0,0 @@
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained false -o "D:\_ASD\"
-pause
